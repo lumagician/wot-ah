80ac700e1bfb85691f0cbe0482ea3d52024400a9
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2021-09-02 00:50:48 +0200

    first marker added and geojson working

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 *Just a random idea I worked on cause I had some time to spend...*
+
 *DISCLAIMER: IM NOT ABLE TO TAKE ANY RESPONSIBILITY FOR THE DATA PROVIDED ON THIS SITE*
 
 # Wot-Ah
 
 Wot-Ah provides you with locations of fountains or other locations you can get drinking water from.
+
 I called it "Wot-Ah" cause it sounds like a british person saying "water".
+
 Thaught I could be kinda funny :)
 
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // Making the map fixed height
 // document.getElementById("issMap").style.height = document
 
+var lat = 0;
+var lon = 0;
+
 $(document).ready(function() {
   header_height = $(".header").height();
   footer_height = $(".footer").height();
@@ -19,4 +22,20 @@ $(document).ready(function() {
   const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   const tiles = L.tileLayer(tileUrl, { attribution });
   tiles.addTo(map);
+
+  getFountains(map);
+
+  var marker = L.marker([lon, lat]).addTo(map);
 })
+
+function getFountains(map) {
+  $.getJSON("/public/fountains.geojson", function(data) {
+    console.log(data.features[0].geometry.coordinates[0])
+    console.log(data.features[0].geometry.coordinates[1])
+
+    lat = data.features[0].geometry.coordinates[0];
+    lon = data.features[0].geometry.coordinates[1];
+
+    var marker = L.marker([lon, lat]).addTo(map);
+  })
+}
